dcbb2bea7efacb21d4abdd74973a4c7962a85913
Alejandro <alejandro@admetricks.com>
2021-04-13 02:49:35 -0400

    fix: rollercoaster gif

--- src/components/PoweredBy.vue
+++ src/components/PoweredBy.vue
@@ -2,5 +2,8 @@
   <p>
     Powered by
     <a href="https://pro.coinbase.com/" target="_blank" class="font-bold text-blue-600">Coinbase Pro</a>
+    <br>
+    Made by
+    <a href="https://alejandro-lopez.dev/" target="_blank" class="font-bold text-yellow-600">Alejandro López</a>
   </p>
 </template>
--- src/components/RollercoasterGif.vue
+++ src/components/RollercoasterGif.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rollercoaster" :class="'rollercoaster--' + direction">
     <img
-      src="https://i0.wp.com/www.diariobitcoin.com/wp-content/uploads/2015/05/bitcoin-tren.gif?resize=415%2C477"
+      src="../assets/bitcoin-rollercoaster.gif"
       alt="Rollercoaster gif"
       class="h-5/6 md:h-full"
     />
