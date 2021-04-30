c6a50d0356043c50173a34adfe464817417c6cc8
Alejandro <alejandro@admetricks.com>
2021-04-30 13:34:11 -0400

    feat(PWA): configure PWA

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,21 @@
 //vue.config.js
+const pwa = {
+  theme_color: "#EE0003",
+  background_color: "#ffffff",
+  display: "browser",
+  scope: "/",
+  start_url: "/",
+  name: "Bitcoin Rollercoaster - Live updates",
+  short_name: "Bitcoin Rollercoaster",
+  description:
+    "Get realtime updates on the Bitcoin price. You also get to see our Rollercoaster Buddy having fun!"
+};
 module.exports = {
-  chainWebpack: (config) => {
-    config.plugin("html").tap((args) => {
+  pwa,
+  chainWebpack: config => {
+    config.plugin("html").tap(args => {
       args[0].title = "Bitcoin Rollercoaster";
       return args;
     });
-  },
+  }
 };
