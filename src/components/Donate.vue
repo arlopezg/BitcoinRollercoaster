6b13fe54f0a011482d19123d16cff15fac04423c
Alejandro <alejandro@admetricks.com>
2021-05-01 17:51:04 -0400

    fix: create QR component

--- src/components/Donate.vue
+++ src/components/Donate.vue
@@ -4,19 +4,25 @@
       :href="'bitcoin:' + publicAddress"
       target="_blank"
       rel="noopener noreferrer"
-      ><img src="https://i.imgur.com/PUlUEFh.png" class="w-10 h-10"
-    /></a>
+    >
+      <QRCode :value="publicAddress" class="w-10 h-10" />
+    </a>
     <figcaption>
       <span class="text-xs">Consider making a donation 🥳</span>
       <br />
-      <a :href="'bitcoin:' + publicAddress" class="text-sm text-blue-500">{{ publicAddress }}</a>
+      <a :href="'bitcoin:' + publicAddress" class="text-sm text-blue-500">{{
+        publicAddress
+      }}</a>
     </figcaption>
   </figure>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
+import QRCode from "./QRCode";
+
 export default {
+  components: { QRCode },
   data() {
     return {
       publicAddress: process.env.VUE_APP_BTC_DONATION_ADDRESS || "unset",
---
+++ src/components/QRCode.vue
@@ -0,0 +1,26 @@
+<template>
+  <img :src="url" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: String, required: true },
+    size: { type: Number, default: 100 },
+  },
+  data() {
+    return { url: null };
+  },
+  methods: {
+    getQrURL(value = "") {
+      const baseURL = "https://chart.googleapis.com/chart?";
+      const size = [this.size, this.size].join("x");
+      const params = `chs=${size}&cht=qr&chl=${value}&choe=UTF-8`;
+      return baseURL + params;
+    },
+  },
+  mounted() {
+    this.url = this.getQrURL(this.value);
+  },
+};
+</script>
